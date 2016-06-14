29eaf2d1c346dc1426b3bb80cf8d87c88598cc34
nathan <nathan@jones.one>
2016-06-14 16:51:55 -0400

    Decided to user polygon shape as a base for the triangle.
    The polygon shape class is much simpler than trying to implement a
    custom shape using three lines.

--- FloorPlan/src/utils/TriangleShape.java
+++ FloorPlan/src/utils/TriangleShape.java
@@ -1,22 +1,25 @@
 package utils;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Line2D;
 import java.awt.geom.PathIterator;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
+
+//TODO: rename method variables to easier to understand names
 public class TriangleShape implements Shape{
+		
+	int x, y, height;
+	double angle;
 	
-	Line2D.Double lineOne, lineTwo, base;
-	double x, y, height, angle;
+	private Polygon triangle;
 	
 	//Based off of the top point of the triangle, will draw with the base on the right side
-	public TriangleShape(double x, double y, double height, double angle)
+	public TriangleShape(int x, int y, int height, double angle)
 	{
 		this.x = x;
 		this.y = y;
@@ -25,101 +28,82 @@ public class TriangleShape implements Shape{
 		
 		double halfAngle = (angle/2.0);
 		
-		double halfBaseLength = Math.tan(Math.toRadians(halfAngle)) * height;
+		int  halfBaseLength = (int) (Math.tan(Math.toRadians(halfAngle)) * height);
 		
-		
-		
-		lineTwo = new Line2D.Double(x, y, x + height, y - halfBaseLength);
-		lineOne = new Line2D.Double(x, y, x + height, y + halfBaseLength);
-		base = new Line2D.Double(lineTwo.x2, lineTwo.y2, lineOne.x2, lineOne.y2);
-		
-		
-		
+		triangle = new Polygon(new int[] {x, x + height, x + height},new int[] {y, y - halfBaseLength, y + halfBaseLength}, 3);
+	
 	}
 	
 	public void draw(Graphics2D g2D)
 	{
-	    Color tmpColor = g2D.getColor();
-	    g2D.setColor(Color.green);
-	    g2D.draw(lineTwo);
-	    g2D.setColor(Color.MAGENTA);
-	    g2D.draw(lineOne);
-	    g2D.setColor(Color.PINK);
-	    g2D.draw(base);
+	    //Color tmpColor = g2D.getColor();
+	    //g2D.setColor(Color.ORANGE);
 	    
-	    g2D.setColor(tmpColor);
+	    g2D.draw(triangle);
+	    
+	    //g2D.setColor(tmpColor);
 	}
 
 	@Override
 	public boolean contains(Point2D arg0)
 	{
-	    // TODO Auto-generated method stub
-	    return false;
+	    return triangle.contains(arg0);
 	}
 
 	@Override
 	public boolean contains(Rectangle2D arg0)
 	{
-	    // TODO Auto-generated method stub
-	    return false;
+	    return triangle.contains(arg0);
 	}
 
 	@Override
 	public boolean contains(double arg0, double arg1)
 	{
-	    // TODO Auto-generated method stub
-	    return false;
+	    return triangle.contains(arg0, arg1);
 	}
 
 	@Override
 	public boolean contains(double arg0, double arg1, double arg2,
 		double arg3)
 	{
-	    // TODO Auto-generated method stub
-	    return false;
+	    return triangle.contains(arg0, arg1, arg2, arg3);
 	}
 
 	@Override
 	public Rectangle getBounds()
 	{
-	    // TODO Auto-generated method stub
-	    return null;
+	    return triangle.getBounds();
 	}
 
 	@Override
 	public Rectangle2D getBounds2D()
 	{
-	    // TODO Auto-generated method stub
-	    return null;
+	    return triangle.getBounds2D();
 	}
 
 	@Override
 	public PathIterator getPathIterator(AffineTransform arg0)
 	{
-	    // TODO Auto-generated method stub
-	    return null;
+	    return triangle.getPathIterator(arg0);
 	}
 
 	@Override
 	public PathIterator getPathIterator(AffineTransform arg0, double arg1)
 	{
-	    // TODO Auto-generated method stub
-	    return null;
+	    return triangle.getPathIterator(arg0, arg1);
 	}
 
 	@Override
 	public boolean intersects(Rectangle2D arg0)
 	{
-	    // TODO Auto-generated method stub
-	    return false;
+	    return triangle.intersects(arg0);
 	}
 
 	@Override
 	public boolean intersects(double arg0, double arg1, double arg2,
 		double arg3)
 	{
-	    // TODO Auto-generated method stub
-	    return false;
+	    return triangle.intersects(arg0, arg1, arg2, arg3);
 	}
 
 }
